e9c926525898fbf7407674a88302182484d6fd60
椰奶猫猫 <98303287+mazemoe@users.noreply.github.com>
2025-11-12 21:55:10 +0800

    修改作者简介

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '阿椰', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '人生天地，寿非金石。, 银台窃药，想奔月而何年？,金殿煎香，思反魂而无日。', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '思反魂而无日', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://cococat.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '历史同人, 博客', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
