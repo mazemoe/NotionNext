55c25164dd063399f76123c79f4f052714404311
椰奶猫猫 <maze_protonmail@protonmail.com>
2026-02-07 19:09:54 +0800

    删除统计卡

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -19,7 +19,7 @@ const CONFIG = {
   HEXO_MENU_CATEGORY: true, // 显示分类
   HEXO_MENU_TAG: true, // 显示标签
   HEXO_MENU_ARCHIVE: true, // 显示归档
-  HEXO_MENU_SEARCH: true, // 显示搜索
+  HEXO_MENU_SEARCH: false, // 显示搜索
   HEXO_MENU_RANDOM: false, // 显示随机跳转按钮
 
   HEXO_POST_LIST_COVER: false, // 列表显示文章封面
@@ -30,13 +30,13 @@ const CONFIG = {
   HEXO_POST_LIST_PREVIEW: false, // 读取文章预览
   HEXO_POST_LIST_IMG_CROSSOVER: true, // 博客列表图片左右交错
 
-  HEXO_ARTICLE_ADJACENT: false, // 显示上一篇下一篇文章推荐
+  HEXO_ARTICLE_ADJACENT: true, // 显示上一篇下一篇文章推荐
   HEXO_ARTICLE_COPYRIGHT: true, // 显示文章版权声明
   HEXO_ARTICLE_NOT_BY_AI: false, // 显示非AI写作
   HEXO_ARTICLE_RECOMMEND: false, // 文章关联推荐
 
   HEXO_WIDGET_LATEST_POSTS: false, // 显示最新文章卡
-  HEXO_WIDGET_ANALYTICS: true, // 显示统计卡
+  HEXO_WIDGET_ANALYTICS: false, // 显示统计卡
   HEXO_WIDGET_TO_TOP: true,
   HEXO_WIDGET_TO_COMMENT: true, // 跳到评论区
   HEXO_WIDGET_DARK_MODE: true, // 夜间模式
